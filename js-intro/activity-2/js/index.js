272ead793c25544f5e20a1d19b5f598c0f696b60
Andrea Jorge <andrea_jorge814@msn.com>
2018-01-25 09:42:05 -0800

    completed activity 2

--- js-intro/activity-2/js/index.js
+++ js-intro/activity-2/js/index.js
@@ -17,10 +17,41 @@ ctx.strokeStyle = 2;
 let input = document.getElementById('textInput');
 input.onkeydown = drawLines;
 
+function countWordLength(d) {
+    return d.length;
+}
+
+const directions = [
+    { x: 10, y: 0 },
+    { x: 0, y: 10 },
+    { x: -10, y: 0 },
+    { x: 0, y: -10 }
+];
 
 // Function to draw lines
 function drawLines() {
+    let position = {
+        x: width / 2,
+        y: height / 2
+    }
+    ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.moveTo(position.x, position.y);
+    let words = input.value.split(' ');
+    // console.log(words);
+    // get word lengths
+    let wordLens = words.map(countWordLength);
+    // console.log(wordLens);
 
+    // iterate through word lengths 
+    wordLens.forEach(function (d, i) {
+        let dirIndex = i % 4;
+        let currDirection = directions[dirIndex];
+        position.x += currDirection.x * d;
+        position.y += currDirection.y * d;
+        ctx.lineTo(position.x, position.y);
+    })
+
+    ctx.stroke();
 }
-
 
