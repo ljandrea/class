3dfca945268b0f58f7720dd8445206e16112657a
Andrea Jorge <andrea_jorge814@msn.com>
2018-02-08 09:27:04 -0800

    completed activity 1

--- react/activity-1/js/index.js
+++ react/activity-1/js/index.js
@@ -8,9 +8,38 @@ const people = [
 ];
 
 // Class for an individual person
-
+class Person extends React.Component {
+    render() {
+        let name = this.props.name === undefined ? "no name provided" : this.props.name;
+        let interest = this.props.interest === undefined ? "no interest provided" : this.props.interest;
+        // console.log(this.props.name);
+        return (
+            <div>
+                <p>Hello, my name is {name}, and I like {interest}</p>
+            </div>
+        )
+    }
+}
 
 // Class to represent a group of people
+class People extends React.Component {
+    render() {
+        console.log(this.props.group);
+        return (
+            <div>
+                {this.props.group.map((d, i) =>
+                    <Person name={d.name} interest={d.interest} key={'person' + i} />)
+
+                }
+            </div>
+        )
+    }
+
+}
 
 // Render your component in the `main` section
 
+ReactDOM.render(
+    <People group={people} />,
+    document.querySelector('main')
+)
