50392ff1643cc614807fe7d39eab4a6622ef4cc7
Andrea Jorge <andrea_jorge814@msn.com>
2018-02-13 10:10:36 -0800

    js update

--- es6/js/index.js
+++ es6/js/index.js
@@ -1,16 +1,28 @@
 'use strict';
 
-
+let url = 'https://img.buzzfeed.com/buzzfeed-static/static/2015-02/18/10/enhanced/webdr09/enhanced-buzz-32241-1424274656-13.jpg?downsize=715:*&output-format=auto&output-quality=auto';
 // Class Puppy
 class Puppy {
     // Constructor: takes in an img url, and a sound
-
+    constructor(img = url, sound = 'bark') {
+        this.img = img;
+        this.sound = sound;
+    }
     // Method for "speaking" using responsiveVoice
-
+    bark() {
+        responsiveVoice.speak(this.sound);
+    }
     // Render a Div that you can click on to bark
-
+    render() {
+        let puppyCard = $('div');
+        puppyCard.css('background-image', `url(${this.img})`);
+        puppyCard.attr('class', 'puppyCard col-sm-4');
+        puppyCard.click(() => this.bark());
+        return puppyCard;
+    }
 }
-
+let myPuppy = new Puppy('', 'woof');
+myPuppy.bark();
 
 // Class Form
 class PuppyForm {
@@ -52,23 +64,37 @@ class PuppyForm {
 // Class for the app
 class PuppyApp {
     // Constructor: takes in a parent element and list of puppies
-
+    constructor(parentElement, puppyList) {
+        this.parentElement = parentElement;
+        this.puppyList = puppyList;
+    }
     // Add puppy: pushes new data into list of puppies and re-renders the app
-
+    addPuppy(img, sound) {
+        this.puppyList.push({ url: img, sound: sound });
+        this.render();
+    }
 
     // Render;
     render() {
         // Empty parent element
-
+        this.parentElement.empty();
 
         // Create and render a new form
-
+        let myForm = new PuppyForm();
+        let renderedForm = myForm.render();
+        this.parentELmetn.append(renderedForm);
         // Append puppy list element to parent (in a wrapper div)
-
+        let puppyWrapper = $('<div class="row">');
+        this.parentElement.append(puppyWrapper);
+        this.puppyList.map((puppyInfo) => {
+            let newPuppy = new Puppy(puppyInfo.url, puppyInfo.sound);
+            puppyWrapper.append(newPuppy.render());
+        })
     }
 }
 
 // Create a new app with a single puppy
-
+let myApp = new PuppyApp($('#content'));
+myApp.render();
 
 // Render the app
